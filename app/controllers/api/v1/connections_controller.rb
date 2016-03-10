d8198aa554bfd7287d7ce20d1bc85a93abe1442d
ShannonPaige <spaige@gmail.com>
2016-03-09 21:47:11 -0700

    Update graphing program from flash to js

--- app/controllers/api/v1/connections_controller.rb
+++ app/controllers/api/v1/connections_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::ConnectionsController < ApplicationController
 
   def index
     @connections = Character.find_connection(params[:first_character], params[:second_character])
-    render  json: @connections.as_json, serializer: ::DataSerializer
+    render  json: @connections.as_json, serializer: ::ConnectionSerializer
+    # render  json: @connections.as_json, serializer: ::DataSerializer
   end
 end
--- app/models/connection.rb
+++ app/models/connection.rb
@@ -18,7 +18,8 @@ class Connection
       rel = service.neo4j_edge(relationship)
       source_id = rel[0].gsub('http://localhost:7474/db/data/node/', '')
       target_id = rel[1].gsub('http://localhost:7474/db/data/node/', '')
-      {source: source_id, target: target_id, label: rel[2]}
+      {from: source_id, to: target_id }
+      # {source: source_id, target: target_id, label: rel[2]}
     end
   end
 
