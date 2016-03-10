ee16e5c7cb8b9228c618ec7acc0cf0bc11c9f4b5
ShannonPaige <spaige@gmail.com>
2016-03-09 23:49:00 -0700

    update service params

--- app/services/comic_vine_service.rb
+++ app/services/comic_vine_service.rb
@@ -5,11 +5,14 @@ class ComicVineService
     @comic_vine_connection = Faraday.new(url: 'http://comicvine.gamespot.com/api/') do |faraday|
       faraday.adapter  Faraday.default_adapter
       faraday.params[:api_key] = ENV['comic_vine_key']
+      faraday.params[:format] = json
+      faraday.params[:resources] = "character"
+      faraday.params[:field_list] = "id,image,deck,character_enemies,character_friends"
     end
   end
 
   def get_character(id)
-    parse(comic_vine_connection.get("character/4005-#{id}/?format=json&resources=character&field_list=id,image,deck,character_enemies,character_friends"))[:results]
+    parse(comic_vine_connection.get("character/4005-#{id}"))[:results]
   end
 
 
